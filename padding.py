71c5950099196d0db1dc4d04a40215b77c34b245
mikelv21 <a01706424@itesm.mx>
2021-05-06 12:09:05 -0500

    Corregir consideraciones para padding

--- padding.py
+++ padding.py
@@ -1,15 +1,22 @@
+#Padding by Jose Miguel Luna Vega
+
 #Libreria para manejar matrices
 import numpy as np
 
-#Funcion para hacer el padding recibiendo una matriz y el valor 
-def padding(mat, p):
-    row, col = mat.shape
-    matP = np.zeros((row+p*2,col+p*2))
-    matP[p:matP.shape[0]-p, p:matP.shape[1]-p] = mat
+#Funcion para hacer el padding recibiendo una matriz y un kernel 
+def padding(mat, kernel):
+    row_k, col_k = kernel.shape
+    row_m, col_m = mat.shape
+
+    pad_height = int((row_k - 1) / 2)
+    pad_width = int((row_k - 1) / 2)
+    
+    matP = np.zeros((row_m+pad_height*2,col_m+pad_width*2))
+    matP[pad_height:matP.shape[0]-pad_height, pad_width:matP.shape[1]-pad_width] = mat
     return matP
 
-#Ejemplo de prueba con padding = 1 
-p = 1
+#Ejemplo de prueba 
+kernel = np.matrix(' 1,1,1 ; 0,0,0 ; 2,10,3 ')
 originalMatrix = np.matrix('1,1,1; 0,0,0 ; 2,10,3')
-matrixPadded = padding(originalMatrix,p)
+matrixPadded = padding(originalMatrix,kernel)
 print(matrixPadded)
