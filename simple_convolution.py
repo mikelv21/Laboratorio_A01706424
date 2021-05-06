6f72e3db3828bf3eadd9a46112b88cd7a23c362f
mikelv21 <a01706424@itesm.mx>
2021-05-05 20:42:51 -0500

    Arregla el nombre del archivo

--- simple_convolution.py
+++ simple_convolution.py
@@ -1,6 +1,9 @@
 #Convolution Simple by Jose Miguel Luna Vega
+
+#Libreria para mostrar la matriz resultante
 import matplotlib.pyplot as plt
 
+#Declaracion para dos ejemplos de convolucion
 matrix=[ [1,2,3,4,5,6],
           [7,8, 9, 10, 11,12],
           [0, 0, 1,16,17,18],
@@ -21,6 +24,7 @@ filter2=[ [1,0,1],
          [0,0,0],
          [1,0,3] ]
 
+#Funcion auxiliar para los calculos aritmeticos de la convolucion
 def conv_helper (mat, fil):
     res = int()
     for i in range(len(fil)):
@@ -28,6 +32,7 @@ def conv_helper (mat, fil):
             res += mat[i][j]*fil[i][j]
     return res
 
+#Funcion auxiliar para encontrar la matriz a operar dentro de la imagen
 def create_matrix (matrix, col, fil,a,b):
     aux=[]
     for i in range (fil):
@@ -37,6 +42,7 @@ def create_matrix (matrix, col, fil,a,b):
         aux.append(fila)
     return (aux)
 
+#Ejercicio de convolucion
 def convolution (mat, kernel, padding, strides):
     res = []
     a = 0
@@ -57,13 +63,12 @@ def convolution (mat, kernel, padding, strides):
 padding = 0
 strides = 1
 
+#Impresion de las matrices resultantes
 print(convolution(matrix,filter, padding, strides))
 print(convolution(matrix2,filter2, padding, strides))
- 
 plt.imshow(convolution(matrix,filter, padding, strides), cmap='gray')
 plt.title("Output Image using {}X{} Kernel".format(len(filter), len(filter[1])))
 plt.show()
-
 plt.imshow(convolution(matrix2,filter2, padding, strides), cmap='gray')
 plt.title("Output Image 2 using {}X{} Kernel".format(len(filter2), len(filter2[1])))
 plt.show() 
